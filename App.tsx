bc8a225ca8cc0b722bab1e759a23e55f4c11f41a
coutojeferson <coutojefersonn@gmail.com>
2024-02-25 12:49:28 -0300

    Inserido integração com atlas e mongo DB

--- App.tsx
+++ App.tsx
@@ -6,23 +6,31 @@ import {
   Roboto_400Regular,
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
+
 import { Loading } from './src/components/Loading';
 import { StatusBar } from 'react-native';
 
+import { AppProvider, UserProvider } from '@realm/react';
+import { REALM_APP_ID } from '@env';
+import { Home } from './src/screens/Home';
+
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
-
   if (!fontsLoaded) {
     return <Loading />;
   }
   return (
-    <ThemeProvider theme={theme}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-      <SignIn />
-    </ThemeProvider>
+    <AppProvider id={REALM_APP_ID}>
+      <ThemeProvider theme={theme}>
+        <StatusBar
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+        <UserProvider fallback={SignIn}>
+          <Home />
+        </UserProvider>
+      </ThemeProvider>
+    </AppProvider>
   );
 }
