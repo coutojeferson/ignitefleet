dee89b9d4842a721bf14d0b19022119bbb4a960d
coutojeferson <coutojefersonn@gmail.com>
2024-03-01 08:55:35 -0300

    Criação do componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+import { TouchableOpacity } from 'react-native';
+import { Container, Title } from './styles';
+import { ArrowLeft } from 'phosphor-react-native';
+import theme from '../../theme';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
+
+type Props = {
+  title: string;
+};
+
+export function Header({ title }: Props) {
+  const insets = useSafeAreaInsets();
+  const { goBack } = useNavigation();
+
+  const paddingTop = insets.top + 42;
+  return (
+    <Container style={{ paddingTop }}>
+      <TouchableOpacity activeOpacity={0.7} onPress={goBack}>
+        <ArrowLeft size={24} weight="bold" color={theme.COLORS.BRAND_LIGHT} />
+      </TouchableOpacity>
+
+      <Title>{title}</Title>
+    </Container>
+  );
+}
